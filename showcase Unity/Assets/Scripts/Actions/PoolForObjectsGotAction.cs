54226e32e0f4a5e7a6735b6cbc1e9e1412fa8f31
plexov01 <plexov.01@mail.ru>
2023-08-11 14:13:27 +0700

    fixed bug with PoolForObjectsGotAction
    - Added coroutine for waiting the sign method

--- showcase Unity/Assets/Scripts/Actions/PoolForObjectsGotAction.cs
+++ showcase Unity/Assets/Scripts/Actions/PoolForObjectsGotAction.cs
@@ -4,12 +4,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+
 public class PoolForObjectsGotAction : MonoBehaviour
 {
     //Action should be active after initialization
     public static Action<GameObject> PoolForObjectsGot;
+
+    private ObjectPool _objectPool;
+    
+    IEnumerator WaitForSign()
+    {
+        yield return new WaitWhile(()=>!_objectPool.GetStateActionSign());
+        PoolForObjectsGot?.Invoke(gameObject);
+    }
     private void Start()
     {
-        PoolForObjectsGot?.Invoke(gameObject);
+        _objectPool = Resources.Load<ObjectPool>("ScriptableObjects/ObjectPool");
+        //waiting the sign of action
+        StartCoroutine(WaitForSign());
+
     }
 }
--- showcase Unity/Assets/Scripts/Managers/ObjectPool.cs
+++ showcase Unity/Assets/Scripts/Managers/ObjectPool.cs
@@ -12,10 +12,15 @@ public class ObjectPool : ScriptableObject
    private List<GameObject> _pooledObjects = new List<GameObject>();
    
    private GameObject _poolForObjects;
+   private bool _actionsSigned = false;
    
    public void GetPoolForObjects(GameObject poolForObjects)
    {
       _poolForObjects = poolForObjects;
+      if (_poolForObjects==null)
+      {
+         Debug.Log($"Trying to get poolForObjects = {_poolForObjects}, but something goes wrong");
+      }
    }
    
    public GameObject GetObjectFromPool(string objectName)
@@ -35,23 +40,27 @@ public class ObjectPool : ScriptableObject
       var prefab = PrefabsForPool.FirstOrDefault(obj => obj.name == objectName);
       if (prefab != null)
       {
-
          // Create a new instance
-         GameObject newInstace = Instantiate(prefab, Vector3.zero, Quaternion.identity,_poolForObjects.transform);
+         GameObject newInstance = Instantiate(prefab, Vector3.zero, Quaternion.identity,_poolForObjects.transform);
          
          // Make sure you set it's name (so you remove the Clone that Unity ads)
-         newInstace.name = objectName;
+         newInstance.name = objectName;
 
          // Set it's position to zero
-         newInstace.transform.localPosition = Vector3.zero;
+         newInstance.transform.localPosition = Vector3.zero;
          
-         _pooledObjects.Add(newInstace);
+         _pooledObjects.Add(newInstance);
          
-         return newInstace;
+         return newInstance;
       }
       
       Debug.LogWarning("Object pool doesn't have a prefab for the object with name " + objectName);
       return null;
+   }
+   //return the state of action sign
+   public bool GetStateActionSign()
+   {
+      return _actionsSigned;
    }
    //function will do after scene loading
    private void CleanPooledObjects()
@@ -61,14 +70,17 @@ public class ObjectPool : ScriptableObject
 
    private void OnEnable()
    {
-      StartSceneAction.SceneStarted += CleanPooledObjects;
       PoolForObjectsGotAction.PoolForObjectsGot += GetPoolForObjects;
+      StartSceneAction.SceneStarted += CleanPooledObjects;
+      _actionsSigned = true;
+
    }
 
    private void OnDisable()
    {
       StartSceneAction.SceneStarted -= CleanPooledObjects;
       PoolForObjectsGotAction.PoolForObjectsGot -= GetPoolForObjects;
+      _actionsSigned = false;
    }
    
 }
